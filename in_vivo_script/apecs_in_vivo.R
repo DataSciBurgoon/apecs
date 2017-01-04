d80b8d1938e5319d830593eb720928b5f57d785b
DataSciBurgoon <burgoon.lyle@gmail.com>
2017-01-04 16:18:43 -0500

    had to change the mirror -- on testing on windows the rstudio mirror didn't work well. changed to duke mirror.

--- in_vivo_script/apecs_in_vivo.R
+++ in_vivo_script/apecs_in_vivo.R
@@ -57,7 +57,7 @@ list_of_packages <- c("ggplot2", "h2o")
 #If not, then install them.
 
 new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
-if(length(new_packages)) install.packages(new_packages, repos='http://cran.rstudio.com')
+if(length(new_packages)) install.packages(new_packages, repos='http://archive.linux.duke.edu/cran/')
 library(ggplot2)
 library(h2o)
 #library(gRain)
